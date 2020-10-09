246b8565443da1852064ad3970f3836d0e3960ba
Кирилл Марченко <71129480+KiRiLMarchenko@users.noreply.github.com>
2020-10-09 13:28:56 +0300

    Add files via upload

---
+++ ЛР3  УПР1.cpp
@@ -0,0 +1,19 @@
+	#include <stdio.h>
+	#include <locale.h>
+	#include <math.h>
+	int main (void)
+	{	
+		setlocale(LC_ALL,"RUSSIAN");
+		float m,r,g;
+		printf("¬ведите переменные r,m ,M>R\n");
+		scanf("%f %f\n",&r,&m);
+		g=0;
+		while(r<=m)
+		{	
+			r=r+r*0.04;
+			g=g+1;
+		}
+		printf("„ерез %f\n",g);
+	
+		return 0;	
+	}
